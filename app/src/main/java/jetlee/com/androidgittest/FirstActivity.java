9a75d87c89bff1a0fa55dc5b9ca1f8758bff5214
jetLee92 <940077208@qq.com>
2018-03-21 16:04:48 +0800

    åå»ºFirstActivityåactivity_first

---
+++ app/src/main/java/jetlee/com/androidgittest/FirstActivity.java
@@ -0,0 +1,13 @@
+package jetlee.com.androidgittest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/3/21.
+ */
+
+public class FirstActivity extends AppCompatActivity {
+
+
+
+}
